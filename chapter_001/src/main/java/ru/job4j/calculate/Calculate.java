c664b5774bf6a3c1d00cea02c528f19dbb964a03
voroninalexey <voronin_alex@bk.ru>
2019-03-30 14:49:55 +0300

    1.3. JavaDoc. Code convention[#124583]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,10 +1,18 @@
 
 package ru.job4j.calculate;
-
+/**
+  * Calculate класс для вычисления арифметических операций
+  * @author alexvoronin
+  * @since 30.03.2019
+  * @version 1
+  */  
 public class Calculate {
-	
+	/** 
+	* Конструктор, вывод строки в консоль.
+	* @param args - args
+	*/
 public static void main (String[] args) {
-System.out.println("hello world"); 
+   System.out.println("hello world"); 
 }
 
 }
