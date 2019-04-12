ef580c7e5b58c397ac2272b5875a0568c9a263df
voroninalexey <voronin_alex@bk.ru>
2019-04-12 10:03:03 +0300

    4.3. Вычисление площади треугольника[#124546]

---
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ *@author Alex Voronin
+ *@since 12.04.2019
+ *@version 1
+ */
+public class TriangleTest {
+    /**
+     * тест вычисления площади треугольника
+     */
+    @Test
+    public void whenAreaSetThreePointsThenTriangleArea() {
+        // Создаем объект треугольник.
+        Triangle triangle = new Triangle();
+        // Вычисляем площадь.
+        double result = triangle.area(0, 0, 0, 2, 2, 0);
+        // Задаем ожидаемый результат.
+        double expected = 2D;
+        //Проверяем результат и ожидаемое значение.
+        assertThat(result, closeTo(expected, 0.1));
+    }
+}
